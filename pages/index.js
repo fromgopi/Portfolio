709c9b03c09ab60b4a5200a949223ad4d381fe4d
Gopi Krishna M <gopt.muktevi@gmail.com>
2021-05-18 10:44:15 +0530

    WIP: Adding some tailwind and testing some components

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+// import image from '../pages/img/store.jpg';
 
 export default function Home() {
   return (
-    <div>Hello Welcome to the world of Oscarmild.</div>
+    <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+      <div className="md:flex">
+        <div className="md:flex-shrink-0">
+          <img className="w-32 h-32 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" 
+                src="/store.jpg" alt="Man looking at item at a store" />
+        </div>
+      </div>
+    </div>
   )
 }
