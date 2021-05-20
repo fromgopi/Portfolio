10e7bdc6cb946da56404bcc575756a6bacbb4dd8
Gopi Krishna M <gopt.muktevi@gmail.com>
2021-05-20 19:37:26 +0530

    Full blown React, Webpack and Tailwindcss setup inPlace

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './app.styles.scss';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                Welcome to the world of Oscarmild
+            </div>
+        )
+    }
+}
