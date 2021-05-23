698aaf45192c5f9eeace212da697fbc168321387
Gopi Krishna M <gopt.muktevi@gmail.com>
2021-05-23 22:41:17 +0530

    Basic outline in place

--- src/component/hooks/useDarkMode.js
+++ src/component/hooks/useDarkMode.js
@@ -1,5 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 
 const useDarkMode = () => {
     let [theme, setTheme] = useState('light')
+
+    const toggleTheme = useCallback(() => {
+            if (theme === 'light') {
+                localStorage.setItem('os-theme', 'dark')
+                setTheme('dark')
+            } else {
+                localStorage.setItem('os-theme', 'light')
+                setTheme('light')
+            }
+        },
+        [theme]
+    )
+
+    useEffect( () => {
+        const localTheme = localStorage.getItem('os-theme')
+        if (localTheme){
+            setTheme(localTheme)
+        }
+    }, []);
+
+    return [theme, toggleTheme];
 }
+
+export default useDarkMode;
